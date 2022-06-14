baff61101c208a44e0b269eefa12f791e80caeca
이성열 <type101315@gmail.com>
2022-06-15 00:32:07 +0900

    Container 추가

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import './App.css';
 import Header from './header/NavHeader';
+import Container from './container/Container';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import NotFound from './notFound/NotFound';
 
 function App() {
   return (
     <Router>
-      <Header/>
-      <Routes>
-        <Route path="/" element={<p>home</p>} />
-        <Route path="/dev" element={<p>dev</p>} />
-        <Route path="/dict" element={<p>dict</p>} />
-        <Route path="/stories" element={<p>stories</p>} />
-        <Route path="*" element={<NotFound/>} />
-      </Routes>
+      <Header />
+      <Container>
+        <Routes>
+          <Route path="/" element={<p>home</p>} />
+          <Route path="/dev" element={<p>dev</p>} />
+          <Route path="/dict" element={<p>dict</p>} />
+          <Route path="/stories" element={<p>stories</p>} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Container>
     </Router>
   );
 }
---
+++ src/container/Container.js
@@ -0,0 +1,9 @@
+import "./Container.css"
+
+export default function Container({ children }) {
+    return (
+        <div id="container">
+            { children }
+        </div>
+    )    
+}
