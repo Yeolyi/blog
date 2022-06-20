89c01881c8a2e9cf5ace36618f14a81d7871516d
Yeolyi <type101315@gmail.com>
2022-06-20 15:55:08 +0900

    Contrinution graph 추가

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,8 +1,34 @@
 import "./Home.css"
+import GitHubCalendar from "react-github-calendar"
 
 export default function Home() {
+    const selectLastHalfYear = contributions => {
+        const currentYear = new Date().getFullYear();
+        const currentMonth = new Date().getMonth();
+        const shownMonths = 4;
+      
+        return contributions.filter(day => {
+          const date = new Date(day.date);
+          const monthOfDay = date.getMonth();
+      
+          return (
+            date.getFullYear() === currentYear &&
+            monthOfDay > currentMonth - shownMonths &&
+            monthOfDay <= currentMonth
+          );
+        });
+      };
+
     return <>
         <img src="me.jpg"></img>
+        <GitHubCalendar 
+        username="yeolyi" 
+        transformData={selectLastHalfYear} 
+        hideColorLegend
+        hideMonthLabels
+        blockSize={16}
+        blockRadius={8}
+        />
         <div id="links">
         <a href="https://www.instagram.com/studyeolyi/">Instagram</a>
         <a href="https://github.com/Yeolyi">Github</a>
