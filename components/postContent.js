af3627c235d2f3b6e40b2e785580f7834718e7f8
이성열 <type101315@gmail.com>
2022-06-25 21:31:00 +0900

    code highlighter 추가

--- components/postContent.js
+++ components/postContent.js
@@ -1,7 +1,20 @@
 import ReactMarkdown from "react-markdown"
 import Image from "next/image"
 import styles from "./postContent.module.css"
-import { useRouter } from "next/router"
+
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter'
+import * as style from 'react-syntax-highlighter/dist/cjs/styles/prism'
+import javascript from 'react-syntax-highlighter/dist/cjs/languages/prism/javascript'
+import swift from 'react-syntax-highlighter/dist/cjs/languages/prism/swift'
+import java from 'react-syntax-highlighter/dist/cjs/languages/prism/java'
+import c from 'react-syntax-highlighter/dist/cjs/languages/prism/c'
+import cpp from 'react-syntax-highlighter/dist/cjs/languages/prism/cpp'
+
+SyntaxHighlighter.registerLanguage('javascript', javascript)
+SyntaxHighlighter.registerLanguage('swift', swift)
+SyntaxHighlighter.registerLanguage('java', java)
+SyntaxHighlighter.registerLanguage('c', c)
+SyntaxHighlighter.registerLanguage('cpp', cpp)
 
 export default function PostContent({ path, children }) {
     return (
@@ -9,21 +22,37 @@ export default function PostContent({ path, children }) {
             <ReactMarkdown
                 components={{
                     img: image => {
-                        return <Image src={"/images/"+image.src} alt="" title="" width="100%" height="50%" layout="responsive" objectFit="contain"/>
+                        return <Image src={"/images/" + image.src} alt="" title="" width="100%" height="50%" layout="responsive" objectFit="contain" />
+                    },
+                    // https://github.com/remarkjs/react-markdown#use-custom-components-syntax-highlight
+                    code({ node, inline, className, children, ...props }) {
+                        const match = /language-(\w+)/.exec(className || '')
+                        return !inline && match ? (
+                            <SyntaxHighlighter
+                                style={style.prism}
+                                language={match[1]}
+                                PreTag="div"
+                                {...props}
+                            >{String(children).replace(/\n$/, '')}</SyntaxHighlighter>
+                        ) : (
+                            <code className={className} {...props}>
+                                {children}
+                            </code>
+                        )
                     }
                 }}
-                transformLinkUri={ x => {
+                transformLinkUri={x => {
                     if (path) {
                         if (x.startsWith("http")) {
                             return x
                         }
-                        return "/dict/"+path.join("/")+"/"+x;
+                        return "/dict/" + path.join("/") + "/" + x;
                     } else {
                         return x;
                     }
                 }}
             >
-                { children }
+                {children}
             </ReactMarkdown>
         </div>
     )
