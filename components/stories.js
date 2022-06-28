f688c44227c9cd9c9a5b5ac0a2b021ad0d6f9406
이성열 <type101315@gmail.com>
2022-06-29 00:52:59 +0900

    navBar css 수정

--- components/stories.js
+++ components/stories.js
@@ -2,8 +2,8 @@ import styles from "./stories.module.css"
 import { useState } from "react"
 
 const categories = [
-    { name: "TIL", description: "Today I Learned\n매일 공부한 것들에 대한 간단한 기록" , link: "/stories/til"},
-    { name: "Archive", description: "책과 인터넷에서 찾은\n두고두고 볼만한 것들", link: "/stories/archive" },
+    { name: "TIL", description: "Today I Learned\n매일 간단한 공부 기록" , link: "/stories/til"},
+    { name: "Archive", description: "책과 인터넷에서 찾은\n나중에 다시 볼만한 것들", link: "/stories/archive" },
     { name: "Writing", description: "나만 기록할 수 있는\n나만의 지난 경험들", link: "/stories/writing" },
 ]
 
