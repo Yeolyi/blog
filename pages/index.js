6bff1c0c99ed2fa8eb9563d200993edc6c4b4e57
이성열 <type101315@gmail.com>
2022-07-03 23:03:19 +0900

    react-icons 사용

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import styles from "./index.module.css"
 import Image from "next/image"
 import { useState, useEffect, useRef } from "react"
 import { Transition } from "react-transition-group"
+import { FaGithub, FaInstagram } from "react-icons/fa"
 
 const changingTexts = ["코드로 가치를 만드는", "아이같이 상상하는", "항상 공부하는"]
 
@@ -16,6 +17,13 @@ const transitionStyles = {
   exiting: { opacity: 0 },
   exited: { opacity: 0 },
 };
+
+const categoryList = [
+  { name: "TIL", description: "AAA" },
+  { name: "Archive", description: "AAA" },
+  { name: "Writing", description: "AAA" },
+  { name: "Dict", description: "AAA" },
+]
 
 export default function Home() {
   const [check, setCheck] = useState(0);
@@ -40,25 +48,34 @@ export default function Home() {
 
   return (
     <div id={styles.homeContainer}>
-      <div id={styles.profileImage}>
-        <Image src="/images/me.jpg" alt="" width="150px" height="150px" objectFit="cover"></Image>
+      <div id={styles.mainIntroduction}>
+        <div id={styles.profileImage}>
+          <Image src="/images/me.jpg" alt="" width="150px" height="150px" objectFit="cover"></Image>
+        </div>
+        <h2>{"안녕하세요,\n이성열입니다"}</h2>
+        <Transition in={show} timeout={400} nodeRef={nodeRef}>
+          {state => (
+            <div style={{
+              ...defaultStyle,
+              ...transitionStyles[state]
+            }}>
+              {<h3 className={styles.changingTexts}>{changingTexts[check]}</h3>}
+            </div>
+          )}
+        </Transition>
+        <h3>스카이온의 iOS 개발자입니다.</h3>
+        <div id={styles.links}>
+          <a href="https://www.github.com/yeolyi"><FaGithub /></a>
+          <a href="https://www.instagram.com/studyeolyi"><FaInstagram /></a>
+        </div>
       </div>
-      <h2>{"안녕하세요,\n이성열입니다"}</h2>
-      <Transition in={show} timeout={400} nodeRef={nodeRef}>
-        {state => (
-          <div style={{
-            ...defaultStyle,
-            ...transitionStyles[state]
-          }}>
-            {<h3 className={styles.changingTexts}>{changingTexts[check]}</h3>}
-          </div>
-        )}
-      </Transition>
-      <h3>{"스카이온의 iOS 개발자입니다."}</h3>
-      <li>
-        <a href="https://www.github.com/yeolyi">Github</a>
-        <a href="https://www.instagram.com/studyeolyi">Instagram</a>
-      </li>
+      <div>
+
+      </div>
     </div>
   )
+}
+
+function CategoryDescriptions() {
+
 }
