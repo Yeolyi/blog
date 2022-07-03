ed0f32414e1fc276abb2c3f06253418d5a441fe6
Yeolyi <type101315@gmail.com>
2022-07-03 12:48:07 +0900

    Archive 포스트 헤더 추가

--- pages/archive/[id].js
+++ pages/archive/[id].js
@@ -2,15 +2,30 @@ import { getAllPostIds, getPostData } from "../../lib/posts"
 import styles from "./[id].module.css"
 import PostContent from "../../components/postContent"
 import { archiveTheme } from "../../styles/color"
+import Image from "next/image"
 
 export default function ArchiveContent({ postData }) {
     return <div id={styles.archiveContent}>
-        <div id={styles.archiveHeader} style={{backgroundColor: archiveTheme}}>
-            <h2>{postData.title.replace(/ /g, "\n")}</h2>
-            <h3>{postData.subtitle}</h3>
+        <div id={styles.archiveHeader} style={{ backgroundColor: archiveTheme }}>
+            <div id={styles.imageWrapper}>
+                <Image
+                    src={`/images/${postData.id}.png`}
+                    layout="fill"
+                    objectFit="cover"
+                    alt=""
+                >
+                </Image>
+            </div>
+            <div id={styles.archiveHeaderContent}>
+                <h2>{postData.title.replace(/ /g, "\n")}</h2>
+                <h3>{postData.subtitle}</h3>
+            </div>
         </div>
-        <PostContent>{postData.content}</PostContent>
-    </div>
+
+        <div id={styles.archiveText}>
+            <PostContent>{postData.content}</PostContent>
+        </div>
+    </div >
 }
 
 export function getStaticPaths() {
