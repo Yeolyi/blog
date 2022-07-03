55773f7b751022d018310033581c2e8e85423d7c
이성열 <type101315@gmail.com>
2022-07-04 00:38:37 +0900

    메인화면 디자인 수정

--- pages/index.jsx
+++ pages/index.jsx
@@ -19,6 +19,13 @@ const transitionStyles = {
   exited: { opacity: 0 },
 };
 
+const categoryData = [
+  { title: "TIL", description: "매일의 공부를 간단히 기록합니다", href: "/til" },
+  { title: "Archive", description: "읽거나 찾은 정보를 기록합니다", href: "/archive" },
+  { title: "Writing", description: "제 경험을 기록합니다", href: "/writing" },
+  { title: "Dict", description: "공부한 것들을 기록합니다", href: "/dict" },
+]
+
 export default function Home() {
   const [check, setCheck] = useState(0);
   const [show, setShow] = useState(true);
@@ -42,30 +49,43 @@ export default function Home() {
 
   return (
     <div id={styles.homeContainer}>
-      <div id={styles.mainIntroduction}>
-        <div id={styles.profileImage}>
-          <Image src="/images/me.jpg" alt="" width="150px" height="150px" objectFit="cover"></Image>
-        </div>
-        <h2>{"안녕하세요,\n이성열입니다"}</h2>
-        <Transition in={show} timeout={400} nodeRef={nodeRef}>
-          {state => (
-            <div style={{
-              ...defaultStyle,
-              ...transitionStyles[state]
-            }}>
-              {<h3 className={styles.changingTexts}>{changingTexts[check]}</h3>}
-            </div>
-          )}
-        </Transition>
-        <h3>스카이온의 iOS 개발자입니다.</h3>
-        <div id={styles.links}>
-          <a href="https://www.github.com/yeolyi"><FaGithub /></a>
-          <a href="https://www.instagram.com/studyeolyi"><FaInstagram /></a>
+      <div id={styles.mainIntroductionBackground}>
+        <div id={styles.mainIntroduction}>
+          <div id={styles.profileImage}>
+            <Image src="/images/me.jpg" alt="" width="150px" height="150px" objectFit="cover"></Image>
+          </div>
+          <h2>{"안녕하세요,\n이성열입니다"}</h2>
+          <Transition in={show} timeout={400} nodeRef={nodeRef}>
+            {state => (
+              <div style={{
+                ...defaultStyle,
+                ...transitionStyles[state]
+              }}>
+                {<h3 className={styles.changingTexts}>{changingTexts[check]}</h3>}
+              </div>
+            )}
+          </Transition>
+          <h3>스카이온의 iOS 개발자입니다.</h3>
+          <div id={styles.links}>
+            <a href="https://www.github.com/yeolyi"><FaGithub /></a>
+            <a href="https://www.instagram.com/studyeolyi"><FaInstagram /></a>
+          </div>
         </div>
       </div>
-      <div id={styles.links}>
-          <Link href="/til"><a><strong>Til</strong>에 매일의 공부를 기록합니다.</a></Link>
+      <div id={styles.content}>
+        <CategoryList />
       </div>
     </div>
   )
+}
+
+function CategoryList() {
+  return categoryData.map(x => {
+    return <Link href={x.href} key={x.title}>
+      <div id={styles.categoryList}>
+        <h2>{x.title}</h2>
+        <h3>{x.description}</h3>
+      </div>
+    </Link>
+  })
 }
